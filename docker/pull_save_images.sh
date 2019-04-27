6826d701e6d7fcc08100cd78d3a1e55b996c460b
hyfly233 <hyfly233@outlook.com>
2019-04-27 12:00:00 +0800

    commit

---
+++ docker/pull_save_images.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# 检查参数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <image_list_file>"
+    echo "Example: $0 images.txt"
+    exit 1
+fi
+
+IMAGE_FILE="$1"
+
+# 检查文件是否存在
+if [ ! -f "$IMAGE_FILE" ]; then
+    echo "Error: File $IMAGE_FILE not found!"
+    exit 1
+fi
+
+while IFS= read -r image || [ -n "$image" ]; do
+    # 跳过空行和注释行
+    if [[ -z "$image" || "$image" =~ ^[[:space:]]*# ]]; then
+        continue
+    fi
+
+    # 清理镜像名中的空格
+    image=$(echo "$image" | xargs)
+
+    echo "处理镜像: $image"
+
+    if docker pull "$image"; then
+      echo "  ✓ 拉取成功"
+
+      # 生成文件名（替换特殊字符）
+      filename=$(echo "$image" | sed 's|/|_|g' | sed 's|:|_|g')
+      tar_file="$OUTPUT_DIR/${filename}.tar"
+
+      # 保存为 tar 文件
+      echo "  正在保存为 tar 文件: $tar_file"
+      if docker save -o "$tar_file" "$image"; then
+          echo "  ✓ 保存成功: $tar_file"
+
+          # 显示文件大小
+          size=$(ls -lh "$tar_file" | awk '{print $5}')
+          echo "  文件大小: $size"
+      else
+          echo "  ✗ 保存失败"
+      fi
+    else
+        echo "  ✗ 拉取失败"
+    fi
+
+    echo "  --------------------------------"
+done < "$IMAGE_FILE"
